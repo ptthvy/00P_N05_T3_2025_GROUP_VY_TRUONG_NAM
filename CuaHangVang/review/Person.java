45fdd781f6a3a702edd86cea1dae2a0121bf6d8f
NguyenHoangNam47 <24100136@st.phenikaa-uni.edu.vn>
2025-07-01 14:03:27 +0000

    nam cap nhat

--- CuaHangVang/review/Person.java
+++ CuaHangVang/review/Person.java
@@ -1,10 +1,17 @@
 public class Person {
-private String name; 
+    private String name; 
 
-public String getName() {
-return name; }
-public void setName(String newName) {
-this.name = newName; 
+    public String getName() {
+        return name; }
+    public void setName(String newName) {
+        this.name = newName; 
 }
+    public static void In() {
+        Person p = new Person();
+        p.setName("nam");
+        System.out.println(p.name);
+
+        
+    }
 }
 
