d4b6af293d86998fb1db5096b3a9b45cd1c0ea99
NguyenHoangNam47 <24100136@st.phenikaa-uni.edu.vn>
2025-07-01 09:50:22 +0000

    nam cap nhat

--- CuaHangVang/review/Book.java
+++ CuaHangVang/review/Book.java
@@ -1,18 +1,23 @@
-package CuaHangVang.review;
 
 public class Book {
-String title;
-String author;
-int numPages;
-Book() { } ; 
-public Book(String t, String a, int p) {
-title = t;
-author = a;
-numPages = p;
-}
+    String title;
+    String author;
+    int numPages;
 
-public void In(){
-Book myObj = new Book("a","b",2);
-System.out.println(myObj.title);    
-}
+
+    Book() {
+    }; 
+
+    public Book(String t, String a, int p) {
+        title = t;
+        author = a;
+        numPages = p;
+    }
+
+    public static void In() {
+        Book myObj = new Book("a", "b", 2);
+        Book obj = new Book();
+        System.out.println(myObj.title);
+        System.out.println("Author default::" +obj.author);
+    }
 }
