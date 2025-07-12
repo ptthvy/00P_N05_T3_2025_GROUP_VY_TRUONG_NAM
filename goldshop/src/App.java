76b5ac5b82992bec35bda48d0d16cf9949170c75
ptthvy <24100439@st.phenikaa-uni.edu.vn>
2025-07-12 10:21:22 +0000

    vy cap nhat

---
+++ goldshop/src/App.java
@@ -0,0 +1,7 @@
+
+public class App {
+    public static void main(String[] args) {
+        KhachHang kh = new KhachHang("KH01", "Nguyễn Văn A", "0909999999");
+        System.out.println(kh); 
+    }
+}
