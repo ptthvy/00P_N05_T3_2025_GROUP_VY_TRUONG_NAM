c3fc89dabfde7d2af66c20792b07d4b61b0a594a
ptthvy <24100439@st.phenikaa-uni.edu.vn>
2025-07-12 11:26:55 +0000

    vy cap nhat

--- goldshop/src/App.java
+++ goldshop/src/App.java
@@ -1,7 +1,23 @@
-
 public class App {
     public static void main(String[] args) {
-        KhachHang kh = new KhachHang("KH01", "Nguyễn Văn A", "0909999999");
-        System.out.println(kh); 
+      KhachHang.them(new KhachHang("KH01", "Nguyen A", "0123456789"));
+        KhachHang.sua("KH01", "Nguyen Van A", "0999999999");
+        KhachHang.xoa("KH02"); // không tồn tại
+        System.out.println("=== DANH SÁCH KHÁCH HÀNG ===");
+        KhachHang.hienThiTatCa();
+
+        SanPham.them(new SanPham("SP01", "Vàng 9999", 5000000));
+        SanPham.sua("SP01", "Vàng SJC", 5200000);
+        SanPham.xoa("SP02"); // không tồn tại
+        System.out.println("\n=== DANH SÁCH SẢN PHẨM ===");
+        SanPham.hienThiTatCa();
+
+        GiaoDich.them(new GiaoDich("GD01", "Nguyen Van A", "SP01", 10400000));
+        GiaoDich.sua("GD01", "Nguyen Van A", "SP01", 10500000);
+        GiaoDich.xoa("GD02"); 
+        System.out.println("\n=== DANH SÁCH GIAO DỊCH ===");
+        GiaoDich.hienThiTatCa();
     }
 }
+
+
