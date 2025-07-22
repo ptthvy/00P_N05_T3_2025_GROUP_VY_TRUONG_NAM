73ef0976d873849df003c9512d72795a6dfe134d
ductruong02 <truongnguyenduc2k6@gmail.com>
2025-07-22 11:17:35 +0000

    truong commit

--- goldshop/test/TestQuanLyGiaoDich.java
+++ goldshop/test/TestQuanLyGiaoDich.java
@@ -1,16 +1,14 @@
-import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
 
-public class QuanLyGiaoDichTest {
-
-    public void testHienThiGiaoDichTheoNgay() {
+public class TestQuanLyGiaoDich {
+    public static void testHienThiGiaoDichTheoNgay() {
         QuanLyGiaoDich qlgd = new QuanLyGiaoDich();
 
         qlgd.themGiaoDich(new GiaoDich("GD01", LocalDate.now(), 2_000_000));
-        qlgd.themGiaoDich(new GiaoDich("GD02", LocalDate.now().minusDays(1), 1_000_000));
+        qlgd.themGiaoDich(new GiaoDich("GD02", LocalDate.now().minusDays(1), 1_500_000));
         qlgd.themGiaoDich(new GiaoDich("GD03", LocalDate.now(), 5_000_000));
 
-        qlgd.hienThiGiaoDichTheoNgay(LocalDate.now());  // In ra GD01 và GD03
+        System.out.println("Danh sách giao dịch trong ngày hôm nay:");
+        qlgd.hienThiGiaoDichTheoNgay(LocalDate.now());
     }
 }
--- goldshop/test/TestQuanLySanPham.java
+++ goldshop/test/TestQuanLySanPham.java
@@ -1,20 +1,17 @@
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
-public class QuanLySanPhamTest {
-
-    public void testTimKiemSanPhamTheoTen() {
+public class TestQuanLySanPham {
+    public static void testTimKiemSanPhamTheoTen() {
         QuanLySanPham qlsp = new QuanLySanPham();
 
-        qlsp.themSanPham(new SanPham("SP01", "Chuột Logitech", 500_000));
-        qlsp.themSanPham(new SanPham("SP02", "Bàn phím Cơ", 1_200_000));
-        qlsp.themSanPham(new SanPham("SP03", "Chuột Gaming", 700_000));
+        qlsp.themSanPham(new SanPham("SP01", "  Vang 9999", 500_000));
+        qlsp.themSanPham(new SanPham("SP02", "vang 666", 1_200_000));
+        qlsp.themSanPham(new SanPham("SP03", "Vang tây", 700_000));
 
-        List<SanPham> ketQua = qlsp.timKiemSanPhamTheoTen("chuột");
-
+        List<SanPham> ketQua = qlsp.timKiemSanPhamTheoTen("vang");
+        System.out.println("Kết quả tìm kiếm sản phẩm theo tên 'vang':");
         for (SanPham sp : ketQua) {
-            sp.hienThiThongTin();  
+            sp.hienThi();
         }
     }
 }
