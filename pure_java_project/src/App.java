f510132b17c702738f72dd04f76a072c9f8e2b97
NguyenHoangNam47 <24100136@st.phenikaa-uni.edu.vn>
2025-07-23 07:21:06 +0000

    nam  commit

--- pure_java_project/src/App.java
+++ pure_java_project/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) {
-        testTune.runTest(); 
+        Student s1 = new Student("An", 3.5f);
+        Student s2 = new Student("Binh", 3.8f);
+
+        // So sánh
+        System.out.println("So sánh GPA: " + s1.compareTo(s2));
+
+        // Kiểm tra equals
+        System.out.println("Hai student có cùng GPA? " + s1.equals(s2));
     }
 }
