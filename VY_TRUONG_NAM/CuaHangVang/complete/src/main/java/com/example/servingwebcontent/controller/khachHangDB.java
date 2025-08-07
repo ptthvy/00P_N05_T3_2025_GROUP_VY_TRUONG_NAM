40b559f8fb94e9f08678e925cf2dbf3d08045353
ptthv <phamvy2676@gmail.com>
2025-08-07 13:50:04 +0000

    vy commit

--- VY_TRUONG_NAM/CuaHangVang/complete/src/main/java/com/example/servingwebcontent/controller/khachHangDB.java
+++ VY_TRUONG_NAM/CuaHangVang/complete/src/main/java/com/example/servingwebcontent/controller/khachHangDB.java
@@ -7,7 +7,6 @@ import com.example.servingwebcontent.model.KhachHang;
 public class khachHangDB {
 
     public List<KhachHang> getAllKhachHang() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getAllKhachHang'");
     }
 
--- VY_TRUONG_NAM/CuaHangVang/complete/src/main/java/com/example/servingwebcontent/controller/myDBConnection.java
+++ VY_TRUONG_NAM/CuaHangVang/complete/src/main/java/com/example/servingwebcontent/controller/myDBConnection.java
@@ -3,12 +3,12 @@ package com.example.servingwebcontent.controller;
 public class myDBConnection {
 
     public Object getConnectionInfo() {
-        // TODO Auto-generated method stub
+        
         throw new UnsupportedOperationException("Unimplemented method 'getConnectionInfo'");
     }
 
     public boolean testConnection() {
-        // TODO Auto-generated method stub
+        
         throw new UnsupportedOperationException("Unimplemented method 'testConnection'");
     }
 
